47962609133e71ae207680084c68d8b624623c9f
lilili001 <2861166132@qq.com>
2018-09-12 09:50:05 +0800

    前台订单列表分页

--- Repositories/Eloquent/EloquentSaleOrderRepository.php
+++ Repositories/Eloquent/EloquentSaleOrderRepository.php
@@ -33,7 +33,7 @@ class EloquentSaleOrderRepository extends EloquentBaseRepository implements Sale
      */
     public function all()
     {
-       return Order::where('user_id',user()->id)->orderBy('created_at', 'desc')->get();
+       return Order::where('user_id',user()->id)->orderBy('created_at', 'desc')->paginate(10);
     }
 
     /**
